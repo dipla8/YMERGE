23e4ccb59072453aa3cfdac77e15ccda39c6801e
Konstantinos Oikonomopoulos <146142720+dipla8@users.noreply.github.com>
2025-10-18 19:21:19 +0300

    Delete obj_dir directory

--- obj_dir/Vtop__pch.h.fast.d
+++
@@ -1,9 +0,0 @@
-Vtop__pch.h.fast.gch: Vtop__pch.h \
-  /opt/homebrew/Cellar/verilator/5.034/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.034/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.034/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.034/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.034/share/verilator/include/verilated_funcs.h \
-  Vtop__Syms.h Vtop.h Vtop___024root.h \
-  /opt/homebrew/Cellar/verilator/5.034/share/verilator/include/verilated_timing.h \
-  Vtop_MUX8.h
